60ba558f2662f44d684ae62f9abd6d7180573eac
Francisco Orelvi Santos <orelvi@gmail.com>
2018-06-26 01:01:20 -0400

    Creacion y auto login

--- assets/js/Formulario.js
+++ assets/js/Formulario.js
@@ -1,66 +1,94 @@
-     function Registro() {
-            
-            var nombre= document.getElementById("nombre").value;
-            var email = document.getElementById("email").value;
-            var password = document.getElementById("password").value;
-            var confirmar = document.getElementById("confirmar").value;
-
-            var sendInfo = {
-                name:nombre,
-                email: email,
-                password: password
-            };
-
-            console.log(sendInfo);
-
-            $.ajax({
-                method: "post",
-                url: "http://127.0.0.1:8080/register",
-                DataType: "json",
-                data: JSON.stringify(sendInfo),
-                success: function (data) {
-                    console.log(data);
-                    alert("Usuario Registrado Satisfactoriamente!");
-                },
+var Storage = window.localStorage;
 
 
-                error: function (er) {
-                    alert("Usuario Equivocado");
-                    //console.log(er);
-                }
-            })
+function Registro() {
+
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmar = document.getElementById("confirmar").value;
+
+    var sendInfo = {
+        name: nombre,
+        email: email,
+        password: password
+    };
+
+    console.log(sendInfo);
+
+    $.ajax({
+        method: "post",
+        url: "http://127.0.0.1:8080/register",
+        DataType: "json",
+        data: JSON.stringify(sendInfo),
+        success: function (data) {
+            console.log(data);
+            alert("Usuario Registrado Satisfactoriamente!");
+            Login(data);
+            limpiarFormulario(["nombre", "email", "password", "confirmar"]);
+        },
+
+
+        error: function (er) {
+            alert("Usuario Equivocado");
+            //console.log(er);
         }
+    })
+}
 
 
 
+
+
+function Login(Data) {
+
+    var password = "";
+    var usuario = "";
+
+    if (Data == undefined) {
+
+        password = document.getElementById("password").value;
+        usuario = document.getElementById("usuario").value;
+
+    }
+    else {
         
+        password = Data.password;
+        usuario = Data.email;
+    }
 
-        function Login() {
-            var password = document.getElementById("password").value;
-            var usuario = document.getElementById("usuario").value;
+    
 
-            var sendInfo = {
-                email: usuario,
-                password: password
-            };
+    var sendInfo = {
+        email: usuario,
+        password: password
+    };
 
-            console.log(sendInfo);
+    console.log(sendInfo);
 
-            $.ajax({
-                method: "post",
-                url: "http://127.0.0.1:8080/login",
-                DataType: "json",
-                data: JSON.stringify(sendInfo),
-                success: function (data) {
-                    console.log(data);
-                },
-                
+    $.ajax({
+        method: "post",
+        url: "http://127.0.0.1:8080/login",
+        DataType: "json",
+        data: JSON.stringify(sendInfo),
+        success: function (data) {
+            console.log(data);
+            Storage.setItem("BlogWebApi", JSON.stringify(data));
+            location.href = "index.htm";
+        },
 
-                error: function (er) {
-                    alert("Usuario Equivocado");
-                    //console.log(er);
-                }
-            })
+
+        error: function (er) {
+            alert("Usuario Equivocado");
+            //console.log(er);
         }
+    })
+}
 
-   
+function limpiarFormulario(inputs) {
+
+    inputs.forEach(function (v, i) {
+        document.getElementById(v).value = "";
+    });
+}
+
